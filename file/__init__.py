bd3dad90d35eb15102273fe2914d12c4571a8672
Alex Murzina <alexandra.murzina@solvexia.com>
2020-08-11 17:51:07 +1000

    initial commit

---
+++ file/__init__.py
@@ -0,0 +1,1 @@
+__all__ = ["file_upload_chunks"]
---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="solvexia-python-sdk", 
+    version="0.0.1",
+    author="SolveXia Pty Ltd",
+    author_email="support@solvexia.com",
+    description="A development kit written in Python to access and work SolveXia resources",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/solvexia/solvexia-python-sdk",
+    packages=["apiclient"],
+    license="Apache 2.0",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
