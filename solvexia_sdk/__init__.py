acdda9936c04576d243e821b3395f100b00f0679
William <william.su3011@gmail.com>
2021-03-25 13:35:59 +1100

    module import

---
+++ solvexia_sdk/__init__.py
@@ -0,0 +1,7 @@
+import import_module from importlib
+
+fileAPI = import_module("file.file")
+processAPI = import_module("process.process")
+processRunAPI = import_module("processrun.processrun")
+dataStepsAPI = import_module("datasteps.datasteps")
+tableAPI = import_module("table.table")
