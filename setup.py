b0e0786cb543e17d5a7ffeed66ae273f60a3881a
William <william.su3011@gmail.com>
2021-04-06 17:58:22 +1000

    include all files in package

--- setup.py
+++ setup.py
@@ -5,14 +5,15 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="solvexia_sdk", 
-    version="0.0.1",
+    version="0.0.2",
     author="SolveXia Pty Ltd",
     author_email="support@solvexia.com",
     description="A development kit written in Python to access and work SolveXia resources",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/solvexia/solvexia-python-sdk",
-    packages=["solvexia_sdk", "solvexia_sdk.file"],
+    packages=["solvexia_sdk", "solvexia_sdk.file", "solvexia_sdk.process", "solvexia_sdk.datasteps", "solvexia_sdk.processrun",
+    "solvexia_sdk.table"],
     license="Apache 2.0",
     classifiers=[
         "Programming Language :: Python :: 3",
