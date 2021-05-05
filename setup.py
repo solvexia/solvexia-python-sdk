b64f03f94e32e1a8b79d61ea5a22c28203bd3b47
William <william.su3011@gmail.com>
2021-05-05 14:35:02 +1000

    added required dependencies

--- setup.py
+++ setup.py
@@ -14,6 +14,12 @@ setuptools.setup(
     url="https://github.com/solvexia/solvexia-python-sdk",
     packages=["solvexia_sdk", "solvexia_sdk.file", "solvexia_sdk.process", "solvexia_sdk.datasteps", "solvexia_sdk.processrun",
     "solvexia_sdk.table"],
+    install_requires=[
+        'filesplit>=3.0.2',
+        'readme-renderer>=29.0',
+        'requests>=2.25.1',
+        'requests-toolbelt>=0.9.1',
+    ],
     license="Apache 2.0",
     classifiers=[
         "Programming Language :: Python :: 3",
