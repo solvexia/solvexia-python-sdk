a9d95cb79ade0645e0f68840947cfe16c60c1346
myfoxtail <{ID}+{username}@users.noreply.github.com>
2021-05-12 13:33:03 +1000

    python sdk

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="solvexia_sdk", 
-    version="0.0.1",
+    version="1.0.0",
     author="SolveXia Pty Ltd",
     author_email="support@solvexia.com",
     description="A development kit written in Python to access and work SolveXia resources",
