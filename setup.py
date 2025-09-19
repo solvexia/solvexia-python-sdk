496dce22834239631645672035ad45cf3a53d691
Wai Khant <wai.khant@solvexia.com>
2025-09-19 17:34:54 +1000

    fix version to match in PYPI

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="solvexia_sdk", 
-    version="1.3.1",
+    version="1.3.2",
     author="Solvexia Pty Ltd",
     author_email="support@solvexia.com",
     description="A development kit written in Python to access and work with Solvexia resources",
